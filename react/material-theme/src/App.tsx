4ac202317f677cae4717c580a62d0539f2a601f7
Cody Averett <codyaverett@gmail.com>
2023-03-16 13:46:17 -0500

    Update App.tsx

--- react/material-theme/src/App.tsx
+++ react/material-theme/src/App.tsx
@@ -26,7 +26,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <p>
-          Stoked that my code updats so fast!
+          Stoked that my code updates so fast!
         </p>
         <a
           className="App-link"
